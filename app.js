79664539c06dab8d0fca2cbb679dd62991682ca3
Vinton Teo <taulago@ymail.com>
2019-08-20 16:37:22 -0600

    Create app and app.test files.

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
