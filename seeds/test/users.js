a940c9c4deecb2856a7546374cc71fa4832a68ea
Vinton Teo <taulago@ymail.com>
2019-08-22 00:10:50 -0600

    Create users seed file inside test folder within seeds directory.

---
+++ seeds/test/users.js
@@ -0,0 +1,47 @@
+const users_data = require('../../database/default_data/users_data.js');
+
+exports.seed = knex => {
+  return knex('palettes')
+    .del()
+    .then(() =>
+      knex('projects')
+        .del()
+        .then(() =>
+          knex('users')
+            .del()
+            .then(() => {
+              return Promise.all([
+                knex('users')
+                  .insert(...users_data, 'id')
+                  .then(id => {
+                    return knex('projects')
+                      .insert(
+                        {
+                          user_id: id[0],
+                          name: 'New Project',
+                          description: 'This is an example description'
+                        },
+                        'id'
+                      )
+                      .then(id => {
+                        return knex('palettes')
+                          .insert({
+                            project_id: id[0],
+                            name: 'Example palette',
+                            colors: JSON.stringify([
+                              '#000000',
+                              '#888888',
+                              '#ffffff'
+                            ])
+                          })
+                          .catch(error =>
+                            console.log(`Error seeding data: ${error}`)
+                          );
+                      });
+                  })
+              ]);
+            })
+        )
+    )
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
