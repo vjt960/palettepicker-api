2f47cbdccceefd9f322db9f5908d52f2b9d66383
Vinton Teo <taulago@ymail.com>
2019-08-23 14:40:41 -0600

    Write endpoints according to written tests.

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.locals.title = 'Palette Picker Test';
 
 // app.set('port', process.env.PORT || 3001);
 
-app.get('/', (request, response) => {
-  response.status(200).send("We're going to test all the routes!");
-});
-
 app.post('/api/v1/users', (request, response) => {
   const { username, password } = request.body;
   try {
@@ -37,7 +33,7 @@ app.post('/api/v1/users', (request, response) => {
         .send({ error: 'Username or Password not present in payload' });
     }
   } catch {
-    return response.status(500).send({ error: 'Internal server error.' });
+    return response.sendStatus(500);
   }
 });
 
@@ -59,22 +55,37 @@ app.post('/api/v1/users/new', (request, response) => {
         }
       });
   } catch {
-    return response.status(500).send({ error: 'Internal server error.' });
+    return response.sendStatus(500);
   }
 });
 
 app.get('/api/v1/users/:user_id/projects', (request, response) => {
   const { user_id } = request.params;
   try {
-    database('projects')
-      .where('user_id', user_id)
-      .select('id', 'name')
-      .then(projects => response.status(200).json(projects))
-      .catch(() =>
-        response.status(404).json({ error: `Invalid user_id in params.` })
-      );
+    database
+      .raw(
+        `
+      SELECT projects.id, projects.name AS title, projects.description, 
+      json_agg(json_build_object('id', palettes.id, 'name', palettes.name, 'colors', palettes.colors)) 
+      AS palettes 
+      FROM projects 
+      INNER JOIN palettes ON projects.id = palettes.project_id 
+      WHERE projects.user_id = ${user_id} 
+      GROUP BY projects.id
+    `
+      )
+      .then(data => {
+        if (data.rows.length) {
+          response.status(200).json(data.rows);
+        } else {
+          response
+            .status(404)
+            .json({ error: 'No projects found under user_id.' });
+        }
+      })
+      .catch(() => response.status(404).json({ error: 'Invalid user_id.' }));
   } catch {
-    return response.status(500).send({ error: 'Internal server error.' });
+    return response.sendStatus(500);
   }
 });
 
@@ -91,7 +102,7 @@ app.get('/api/v1/users/:user_id/projects/:project_id', (request, response) => {
         response.status(404).json({ error: 'Invalid values within params.' })
       );
   } catch {
-    return response.status(500).send({ error: 'Internal server error.' });
+    return response.sendStatus(500);
   }
 });
 
@@ -102,11 +113,23 @@ app.post('/api/v1/users/projects/new', (request, response) => {
       database('users')
         .where('id', id)
         .select('id')
-        .then(existingID => {
-          if (existingID.length) {
+        .then(activeID => {
+          if (activeID.length) {
             database('projects')
-              .insert({ user_id: id, name, description }, 'id')
-              .then(id => response.status(201).json(id));
+              .where('user_id', id)
+              .andWhere('name', name)
+              .select()
+              .then(existngName => {
+                if (!existngName.length) {
+                  database('projects')
+                    .insert({ user_id: id, name, description }, 'id')
+                    .then(id => response.status(201).json(id));
+                } else {
+                  response.status(409).json({
+                    error: `${name} already exists. Choose a different name.`
+                  });
+                }
+              });
           } else {
             return response
               .status(422)
@@ -115,17 +138,77 @@ app.post('/api/v1/users/projects/new', (request, response) => {
         });
     } else {
       return response
-        .status(422)
+        .status(404)
         .json({ error: 'User ID not present in payload.' });
     }
   } catch {
-    return response.status(500).send({ error: 'Internal server error.' });
+    return response.sendStatus(500);
+  }
+});
+
+app.patch('/api/v1/users/projects/:id/edit', (request, response) => {
+  const { id } = request.params;
+  const { name, description } = request.body;
+  try {
+    if (name) {
+      database('projects')
+        .where('id', id)
+        .update({ name, description }, ['id', 'name'])
+        .then(obj => {
+          if (obj.length) {
+            return response.status(202).json(...obj);
+          } else {
+            return response
+              .status(404)
+              .json({ error: `Project id: ${id} not found.` });
+          }
+        });
+    } else {
+      return response
+        .status(422)
+        .json({ error: 'Project name not found in payload.' });
+    }
+  } catch {
+    return response.sendStatus(500);
+  }
+});
+
+app.delete('/api/v1/users/:user_id/projects/:id', (request, response) => {
+  const { user_id, id } = request.params;
+  try {
+    database('palettes')
+      .where('project_id', id)
+      .del()
+      .then(() =>
+        database('projects')
+          .where('user_id', user_id)
+          .andWhere('id', id)
+          .del()
+          .then(() => response.status(202).json(Number(id)))
+      );
+  } catch {
+    return response.sendStatus(500);
+  }
+});
+
+app.get('/api/v1/users/:user_id/palettes', (request, response) => {
+  const { user_id } = request.params;
+  try {
+    database('palettes')
+      .join('projects', 'palettes.project_id', 'projects.id')
+      .select('palettes.*')
+      .where('projects.user_id', user_id)
+      .then(palettes => response.status(200).json(palettes));
+  } catch {
+    return response.sendStatus(500);
   }
 });
 
 // app.listen(app.get('port'), () => {
 //   console.log(
-//     `${app.locals.title} is running on http://localhost:${app.get('port')}.`
+//     `${app.locals.title} is running SHIT on http://localhost:${app.get(
+//       'port'
+//     )}.`
 //   );
 // });
 
