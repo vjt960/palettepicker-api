429a7c6c494f9a4ac37376ae8cbf2b2453c4e80e
Vinton Teo <taulago@ymail.com>
2019-08-24 23:34:30 -0600

    Convert endpoints to async/await syntax; successfully pass tests.

--- app.js
+++ app.js
@@ -10,23 +10,21 @@ app.locals.title = 'Palette Picker Test';
 
 // app.set('port', process.env.PORT || 3001);
 
-app.post('/api/v1/users', (request, response) => {
+app.post('/api/v1/users', async (request, response) => {
   const { username, password } = request.body;
   try {
     if (username && password) {
-      database('users')
+      const user = await database('users')
         .where('username', username)
         .andWhere('password', password)
-        .select('id', 'username', 'password')
-        .then(user => {
-          if (user.length) {
-            return response.status(200).json(...user);
-          } else {
-            return response
-              .status(401)
-              .send({ error: 'Incorrect Username or Password.' });
-          }
-        });
+        .select('id', 'username', 'password');
+      if (user.length) {
+        return response.status(200).json(...user);
+      } else {
+        return response
+          .status(401)
+          .send({ error: 'Incorrect Username or Password.' });
+      }
     } else {
       return response
         .status(422)
@@ -37,34 +35,30 @@ app.post('/api/v1/users', (request, response) => {
   }
 });
 
-app.post('/api/v1/users/new', (request, response) => {
+app.post('/api/v1/users/new', async (request, response) => {
   const { username, password } = request.body;
   try {
-    database('users')
+    const takenUser = await database('users')
       .select()
-      .where('username', username)
-      .then(takenUser => {
-        if (takenUser.length) {
-          return response
-            .status(409)
-            .send({ error: `Username: ${username} is already taken.` });
-        } else {
-          database('users')
-            .insert({ username, password }, 'id')
-            .then(id => response.status(201).send(id));
-        }
-      });
+      .where('username', username);
+    if (takenUser.length) {
+      return response
+        .status(409)
+        .send({ error: `Username: ${username} is already taken.` });
+    } else {
+      const id = await database('users').insert({ username, password }, 'id');
+      response.status(201).send(id);
+    }
   } catch {
     return response.sendStatus(500);
   }
 });
 
-app.get('/api/v1/users/:user_id/projects', (request, response) => {
+app.get('/api/v1/users/:user_id/projects', async (request, response) => {
   const { user_id } = request.params;
   try {
-    database
-      .raw(
-        `
+    const data = await database.raw(
+      `
       SELECT projects.id, projects.name AS title, projects.description, 
       json_agg(json_build_object('id', palettes.id, 'name', palettes.name, 'colors', palettes.colors)) 
       AS palettes 
@@ -73,69 +67,70 @@ app.get('/api/v1/users/:user_id/projects', (request, response) => {
       WHERE projects.user_id = ${user_id} 
       GROUP BY projects.id
     `
-      )
-      .then(data => {
-        if (data.rows.length) {
-          response.status(200).json(data.rows);
-        } else {
-          response
-            .status(404)
-            .json({ error: 'No projects found under user_id.' });
-        }
-      })
-      .catch(() => response.status(404).json({ error: 'Invalid user_id.' }));
+    );
+    // .catch(() => response.status(404).json({ error: 'Invalid user_id.' }));
+    if (data.rows.length) {
+      response.status(200).json(data.rows);
+    } else {
+      response.status(404).json({ error: 'No projects found under user_id.' });
+    }
   } catch {
-    return response.sendStatus(500);
+    // return response.sendStatus(500);
+    return response.status(404).json({ error: 'Invalid user_id.' });
   }
 });
 
-app.get('/api/v1/users/:user_id/projects/:project_id', (request, response) => {
-  const { user_id, project_id } = request.params;
-  const projectAttributes = ['id', 'user_id', 'name', 'description'];
-  try {
-    database('projects')
-      .where('user_id', user_id)
-      .andWhere('id', project_id)
-      .select(...projectAttributes)
-      .then(project => response.status(200).json(project))
-      .catch(() =>
-        response.status(404).json({ error: 'Invalid values within params.' })
-      );
-  } catch {
-    return response.sendStatus(500);
+app.get(
+  '/api/v1/users/:user_id/projects/:project_id',
+  async (request, response) => {
+    const { user_id, project_id } = request.params;
+    const projectAttributes = ['id', 'user_id', 'name', 'description'];
+    try {
+      const project = await database('projects')
+        .where('user_id', user_id)
+        .andWhere('id', project_id)
+        .select(...projectAttributes);
+      // .catch(() =>
+      //   response.status(404).json({ error: 'Invalid values within params.' })
+      // );
+      response.status(200).json(project);
+    } catch {
+      // return response.sendStatus(500);
+      return response
+        .status(404)
+        .json({ error: 'Invalid values within params.' });
+    }
   }
-});
+);
 
-app.post('/api/v1/users/projects/new', (request, response) => {
+app.post('/api/v1/users/projects/new', async (request, response) => {
   const { id, name, description } = request.body;
   try {
     if (id) {
-      database('users')
+      const activeID = await database('users')
         .where('id', id)
-        .select('id')
-        .then(activeID => {
-          if (activeID.length) {
-            database('projects')
-              .where('user_id', id)
-              .andWhere('name', name)
-              .select()
-              .then(existngName => {
-                if (!existngName.length) {
-                  database('projects')
-                    .insert({ user_id: id, name, description }, 'id')
-                    .then(id => response.status(201).json(id));
-                } else {
-                  response.status(409).json({
-                    error: `${name} already exists. Choose a different name.`
-                  });
-                }
-              });
-          } else {
-            return response
-              .status(422)
-              .json({ error: `${id} is NOT a valid user ID.` });
-          }
-        });
+        .select('id');
+      if (activeID.length) {
+        const existngName = await database('projects')
+          .where('user_id', id)
+          .andWhere('name', name)
+          .select();
+        if (!existngName.length) {
+          const newID = await database('projects').insert(
+            { user_id: id, name, description },
+            'id'
+          );
+          response.status(201).json(newID);
+        } else {
+          response.status(409).json({
+            error: `${name} already exists. Choose a different name.`
+          });
+        }
+      } else {
+        return response
+          .status(422)
+          .json({ error: `${id} is NOT a valid user ID.` });
+      }
     } else {
       return response
         .status(404)
@@ -146,23 +141,21 @@ app.post('/api/v1/users/projects/new', (request, response) => {
   }
 });
 
-app.patch('/api/v1/users/projects/:id/edit', (request, response) => {
+app.patch('/api/v1/users/projects/:id/edit', async (request, response) => {
   const { id } = request.params;
   const { name, description } = request.body;
   try {
     if (name) {
-      database('projects')
+      const projectObj = await database('projects')
         .where('id', id)
-        .update({ name, description }, ['id', 'name'])
-        .then(obj => {
-          if (obj.length) {
-            return response.status(202).json(...obj);
-          } else {
-            return response
-              .status(404)
-              .json({ error: `Project id: ${id} not found.` });
-          }
-        });
+        .update({ name, description }, ['id', 'name']);
+      if (projectObj.length) {
+        return response.status(202).json(...projectObj);
+      } else {
+        return response
+          .status(404)
+          .json({ error: `Project id: ${id} not found.` });
+      }
     } else {
       return response
         .status(422)
@@ -173,54 +166,57 @@ app.patch('/api/v1/users/projects/:id/edit', (request, response) => {
   }
 });
 
-app.delete('/api/v1/users/:user_id/projects/:id', (request, response) => {
+app.delete('/api/v1/users/:user_id/projects/:id', async (request, response) => {
   const { user_id, id } = request.params;
   try {
-    database('palettes')
+    await database('palettes')
       .where('project_id', id)
-      .del()
-      .then(() =>
-        database('projects')
-          .where('user_id', user_id)
-          .andWhere('id', id)
-          .del()
-          .then(() => response.status(202).json(Number(id)))
-      );
+      .del();
+    await database('projects')
+      .where('user_id', user_id)
+      .andWhere('id', id)
+      .del();
+    response.status(202).json(Number(id));
   } catch {
     return response.sendStatus(500);
   }
 });
 
-app.get('/api/v1/users/:user_id/palettes', (request, response) => {
+app.get('/api/v1/users/:user_id/palettes', async (request, response) => {
   const { user_id } = request.params;
   try {
-    database('palettes')
+    const palettes = await database('palettes')
       .join('projects', 'palettes.project_id', 'projects.id')
       .select('palettes.*')
-      .where('projects.user_id', user_id)
-      .then(palettes => response.status(200).json(palettes));
+      .where('projects.user_id', user_id);
+    response.status(200).json(palettes);
   } catch {
     return response.sendStatus(500);
   }
 });
 
-app.post('/api/v1/users/projects/:project_id/palettes', (request, response) => {
-  const { project_id } = request.params;
-  const { name, colors } = request.body;
-  try {
-    if (name && colors) {
-      database('palettes')
-        .insert({ name, colors: JSON.stringify(colors), project_id }, 'id')
-        .then(id => response.status(201).json(id));
-    } else {
-      return response
-        .status(422)
-        .json({ error: 'name and colors not included in payload.' });
+app.post(
+  '/api/v1/users/projects/:project_id/palettes',
+  async (request, response) => {
+    const { project_id } = request.params;
+    const { name, colors } = request.body;
+    try {
+      if (name && colors) {
+        const newID = await database('palettes').insert(
+          { name, colors: JSON.stringify(colors), project_id },
+          'id'
+        );
+        response.status(201).json(newID);
+      } else {
+        return response
+          .status(422)
+          .json({ error: 'name and colors not included in payload.' });
+      }
+    } catch {
+      return response.sendStatus(500);
     }
-  } catch {
-    return response.sendStatus(500);
   }
-});
+);
 
 // app.listen(app.get('port'), () => {
 //   console.log(
